1b6e65cbcb46213b6069a5d60045085e833f5daf
pharwell-code <p.harwell12@gmail.com>
2026-03-05 10:29:03 -0700

    pokemon by parker yo

--- TVPoke/Pokemon/Magmar.py
+++ TVPoke/Pokemon/Magmar.py
@@ -4,9 +4,9 @@ from TVPoke.BaseClasses.Move import Move
 class Magmar(Fire):
     def __init__(self):
         moves = [
-            Move("Heat Wave", "FIRE", 100),
-            Move("V- Create-", "FIRE", 180),
-            Move("Sacred Fire", "FIRE", 100),
-            Move("Eruption", "FIRE", 200)
+            Move("Heat Wave", "FIRE", 1000),
+            Move("V- Create-", "FIRE", 1800),
+            Move("Sacred Fire", "FIRE", 1000),
+            Move("Eruption", "FIRE", 2000)
         ]
-        super().__init__("Magmar", 120, moves, "magmar.png")
+        super().__init__("Magmar", 500000, moves, "magmar.png")
